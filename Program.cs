0d54b1ea2ad974d6ef0ca5f1cfd6c17f60a03f8d
Hank McCord <hymccord@gmail.com>
2026-05-21 16:46:52 -0400

    Initialize repo

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using System.Diagnostics;
+
+namespace CredentialManager.CredentialProvider;
+
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        Debugger.Launch();
+    }
+}
